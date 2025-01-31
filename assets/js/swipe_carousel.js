ce824fc9c5cfbfe44dea774eee87d3cd40e007a8
Denys Herzhyk <denys.herzhyk@kofax.com>
2025-01-31 18:41:15 +0100

    Updated mouse swipe listeners

--- assets/js/swipe_carousel.js
+++ assets/js/swipe_carousel.js
@@ -9,16 +9,23 @@ class SwipeCarousel extends Carousel {
         super._initListeners();
         this.slideContainer.addEventListener('touchstart', this._handleStart.bind(this), { passive: true });
         this.slideContainer.addEventListener('touchend', this._handleEnd.bind(this));
+
         this.slideContainer.addEventListener('mousedown', this._handleStart.bind(this));
         this.slideContainer.addEventListener('mouseup', this._handleEnd.bind(this));
     }
-    _handleStart(event) {
-        this.startPositionX = event instanceof MouseEvent ? event.pageX : event.changedTouches[0].pageX;
+    _handleStart(e) {
+        e.preventDefault();
+        this.startPosX = e instanceof MouseEvent
+            ? e.pageX
+            : e.changedTouches[0].pageX;
     }
-    _handleEnd(event) {
-        this.endPositionX = event instanceof MouseEvent ? event.pageX : event.changedTouches[0].pageX;
-        if (this.endPositionX - this.startPositionX > 100) this.prevHandler();
-        else if (this.endPositionX - this.startPositionX < -100) this.nextHandler();
+    _handleEnd(e) {
+        this.endPosX = e instanceof MouseEvent
+            ? e.pageX
+            : e.changedTouches[0].pageX;
+
+        if (this.startPosX - this.endPosX > 100) this.nextHandler();
+        if (this.startPosX - this.endPosX < -100) this.prevHandler();
     }
 }
 
